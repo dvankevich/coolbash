e6696f8607073a7fbae0754d8902804e9ce811e8
Dmitry Vankevich <dvankevich@gmail.com>
2011-01-05 15:46:35 +0200

    first commit

---
+++ hello.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE:  hello.sh
+# 
+#         USAGE:  ./hello.sh 
+# 
+#   DESCRIPTION:  программа выводит на экран строку текста :)
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR: Dmitry Vankevich (VDE), dvankevich@gmail.com
+#       COMPANY: BANGOR
+#       CREATED: 05.01.2011 15:41:34 EET
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+echo -e "Hello world!!"
+
