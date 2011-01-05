5a12cb3588314931c29689766a5fddf663b58d32
Dmitry Vankevich <dvankevich@gmail.com>
2011-01-05 17:10:44 +0200

    добавил каталог и два файла в нём

---
+++ subfunctiuns/lib01.sh
@@ -0,0 +1,23 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE:  lib01.sh
+# 
+#         USAGE:  ./lib01.sh 
+# 
+#   DESCRIPTION:  процедура 01 хотя не знаю будут ли нужны в bash процедуры да 
+#		еще и в отдельных подкаталогах
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR: Dmitry Vankevich (VDE), dvankevich@gmail.com
+#       COMPANY: BANGOR
+#       CREATED: 05.01.2011 16:58:04 EET
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+echo "еще раз привет!!!"
---
+++ subfunctiuns/lib02.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE:  lib02.sh
+# 
+#         USAGE:  ./lib02.sh 
+# 
+#   DESCRIPTION:  еще одна псевдопроцедура
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR: Dmitry Vankevich (VDE), dvankevich@gmail.com
+#       COMPANY: BANGOR
+#       CREATED: 05.01.2011 17:00:42 EET
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+echo "еще одна про це дура"
